592df4884e78bac5bd9d29295e54891089daed2d
Gyana Ranjan <gyanaranjanmohanta491@gmail.com>
2026-02-11 08:08:09 +0530

    feat: Set `SidebarProvider` default open state to true.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -61,7 +61,7 @@ export default function DashboardLayout({
   }
 
   return (
-    <SidebarProvider defaultOpen={false}>
+    <SidebarProvider defaultOpen={true}>
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 border-b md:border-none">
